61fbde3d63d24a7d6232770bdcf57c78a59b6c57
Scott2bReal <hoeckers@gmail.com>
2020-09-24 13:41:27 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,160 @@
+from flask import Flask, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from cs50 import SQL
+
+from helpers import login_required, apology
+
+import os
+
+import random
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///rpg_tools.db")
+
+# Default landing page, after logged in
+@app.route("/")
+@login_required
+def index():
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    # When user tries to submit form
+    if request.method == "POST":
+
+        # Check to see if username already exists
+        rows = db.execute("SELECT username FROM users WHERE username = :username", username=request.form.get('user'))
+
+        # If it does, error message!
+        if len(rows) > 0:
+           return apology("Username already exists", 403)
+
+        # If not, put them in the database
+        else:
+            db.execute("INSERT INTO users (username, hash) VALUES (:username, :hash)", 
+                       username=request.form.get('user'), hash=generate_password_hash(request.form.get('pwd'), 
+                                                                      method='pbkdf2:sha256', salt_length=8))
+        
+        return render_template("/login.html")
+
+    # When user navigates to page
+    else:
+        return render_template("register.html")
+
+
+@app.route("/dice", methods=['GET', 'POST'])
+@login_required
+def dice():
+    if request.method == "POST":
+        # Number of dice
+        while True:
+            try:
+                dice = int(request.form.get('dice'))
+                break
+            except ValueError:
+                return apology("How you do this?", 403)
+
+        # Number of sides
+        while True:
+            try:
+                sides = int(request.form.get('sides'))
+                break
+            except ValueError:
+                return apology("How you do this?", 403)
+
+        # Bonus
+        if request.form.get('bonus') == "":
+            bonus = 0
+        else:
+            bonus = int(request.form.get('bonus'))
+
+        # Calculate roll and print each individual for user
+        i = 0
+        total = 0
+        rolls = []
+        while i < dice:
+            roll = random.randint(1, sides)
+            total = total + roll
+            i += 1
+            rolls.append(roll)
+
+        total = total + bonus
+        print(f'Final result: {total}')
+        return render_template("dice.html", rolls=rolls, total=total, i=(i+1))
+
+    else:
+        return render_template("dice.html")
